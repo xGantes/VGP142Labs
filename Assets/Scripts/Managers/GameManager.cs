1d40e7dc97985dce3ec6f1ec8f2b80fb646e2773
xGantes <rgantzdesigns@gmail.com>
2022-04-11 14:39:56 -0400

    Lab1

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+
+    }
+}
